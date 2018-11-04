5fa5a2f9de7ef25985941de9a857085d2dee621d
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-04 00:36:10 -0400

    everything working. no frills

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,50 @@
 var friendsArray = [
     {
         "friendName": "Ahmed",
-        "friendPic": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "friendPic": "",
         "scores": [
-            5,
+            4,
+            3,
+            2,
+            4,
+            3,
+            1,
+            1,
             1,
             4,
-            4,
+            2
+        ]
+    },
+    {
+        "friendName": "Omar",
+        "friendPic": "",
+        "scores": [
+            3,
             5,
             1,
+            1,
+            3,
+            3,
+            2,
             2,
             5,
-            4,
-            1
+            2
+        ]
+    },
+    {
+        "friendName": "Smithy",
+        "friendPic": "",
+        "scores": [
+            3,
+            3,
+            1,
+            1,
+            5,
+            5,
+            1,
+            1,
+            3,
+            2
         ]
     }
 ]
